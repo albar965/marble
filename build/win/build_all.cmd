0babfea6e262fa27ea209b942ed55a6abf38aec2
Alexander Barthel <albar965@mailbox.org>
2022-09-26 18:12:45 +0200

    Corrected build script for Windows.

--- build/win/build_all.cmd
+++ build/win/build_all.cmd
@@ -4,7 +4,7 @@ cd %APROJECTS%\build-marble-release
 
 set PATH=%PATH%;C:\Qt\5.15.2\mingw81_32\bin;C:\Qt\Tools\mingw810_32\bin
 
-cmake -Wno-dev -G "MinGW Makefiles" -DQTONLY=TRUE -DQT5BUILD=TRUE -DWITH_quazip=FALSE -DWITH_libshp=FALSE -DWITH_libgps=FALSE -DWITH_libwlocate=FALSE -DWITH_QtLocation=FALSE -DWITH_liblocation=FALSE -DCMAKE_INSTALL_PREFIX=%APROJECTS%\Marble-release -DCMAKE_BUILD_TYPE=Release ..\marble
+cmake -Wno-dev -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DSTATIC_BUILD=TRUE -DQTONLY=TRUE -DBUILD_MARBLE_EXAMPLES=NO -DBUILD_INHIBIT_SCREENSAVER_PLUGIN=NO -DBUILD_MARBLE_APPS=NO -DBUILD_MARBLE_EXAMPLES=NO -DBUILD_MARBLE_TESTS=NO -DBUILD_MARBLE_TOOLS=NO -DBUILD_TESTING=NO -DBUILD_WITH_DBUS=NO -DMARBLE_EMPTY_MAPTHEME=YES -DMOBILE=NO -DWITH_DESIGNER_PLUGIN=NO -DWITH_Phonon=NO -DWITH_Qt5Location=NO -DWITH_Qt5Positioning=NO -DWITH_Qt5SerialPort=NO -DWITH_ZLIB=NO -DWITH_libgps=NO -DWITH_libshp=NO -DWITH_libwlocate=NO -DCMAKE_INSTALL_PREFIX=%APROJECTS%\Marble-release ..\marble\
 
 mingw32-make -j4
 
@@ -14,7 +14,7 @@ rem Copy all headers into the C:\Projekte\marble-release\include\marble|astro di
 
 mkdir %APROJECTS%\Marble-release\include\astro
 xcopy /I /F /Y %APROJECTS%\build-marble-release\src\lib\astro\astrolib_export.h %APROJECTS%\Marble-release\include\astro
-xcopy %APROJECTS%\marble\src\lib\astro\*.h %APROJECTS%\Marble-release\include\astro
+xcopy /I /F /Y %%APROJECTS%\marble\src\lib\astro\*.h %APROJECTS%\Marble-release\include\astro
 
 mkdir %APROJECTS%\Marble-release\include\marble
 xcopy /I /F /Y %APROJECTS%\build-marble-release\src\lib\marble\marble_export.h %APROJECTS%\Marble-release\include\marble
@@ -42,3 +42,5 @@ xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\osm\*.h %APROJECTS%\Marble-rele
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\projections\*.h %APROJECTS%\Marble-release\include\marble
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\routing\*.h %APROJECTS%\Marble-release\include\marble
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\webpopup\*.h %APROJECTS%\Marble-release\include\marble
+
+pause
