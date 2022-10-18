377e88603b9ec3bd4b90148a8444aa761f686580
Alexander Barthel <albar965@mailbox.org>
2022-10-18 18:04:07 +0200

    Fixed debug build deployment.

---
+++ build/mac/build_all_debug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+cd $APROJECTS/build-marble-debug
+
+/Users/alex/Qt/Tools/CMake/CMake.app/Contents/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_MARBLE_EXAMPLES=NO -DBUILD_INHIBIT_SCREENSAVER_PLUGIN=NO -DBUILD_MARBLE_APPS=NO -DBUILD_MARBLE_EXAMPLES=NO -DBUILD_MARBLE_TESTS=NO -DBUILD_MARBLE_TOOLS=NO -DBUILD_TESTING=NO -DBUILD_WITH_DBUS=NO -DMARBLE_EMPTY_MAPTHEME=YES -DMOBILE=NO -DWITH_DESIGNER_PLUGIN=NO -DWITH_Phonon=NO -DWITH_Qt5Location=NO -DWITH_Qt5Positioning=NO -DWITH_Qt5SerialPort=NO -DWITH_ZLIB=NO -DWITH_libgps=NO -DWITH_libshp=NO -DWITH_libwlocate=NO -DCMAKE_PREFIX_PATH=/Users/alex/Qt/5.15.2/clang_64 -DCMAKE_INSTALL_PREFIX=/Users/alex/Projekte/Marble-debug -DEXEC_INSTALL_PREFIX=/Users/alex/Projekte/Marble-debug ../marble/
+
+make -j4
+
+make install
