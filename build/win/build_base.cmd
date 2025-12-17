d0e8654ef5314465f89f08e4850b8584241a8516
Alexander Barthel <albar965@mailbox.org>
2025-12-17 14:17:28 +0100

    Adapted to Qt 6 build

--- build/win/build_base.cmd
+++ build/win/build_base.cmd
@@ -22,46 +22,49 @@ mkdir "%BUILDDIR%"
 pushd "%BUILDDIR%"
 IF ERRORLEVEL 1 goto :err
 
-cmake -Wno-dev -G "MinGW Makefiles" -DWITH_ZLIB=NO -DCMAKE_BUILD_TYPE=%BUILDTYPE% -DSTATIC_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=%DEPLOYDIR% ..\marble\
+cmake -Wno-dev -G "MinGW Makefiles" -DWITH_ZLIB=NO -DCMAKE_BUILD_TYPE=%BUILDTYPE% -DCMAKE_INSTALL_PREFIX=%DEPLOYDIR% ..\marble\
 IF ERRORLEVEL 1 goto :err
 
-mingw32-make -j4
+cmake --build . -j4
 IF ERRORLEVEL 1 goto :err
 
-mingw32-make install
+cmake --install .
 IF ERRORLEVEL 1 goto :err
-
-rem Copy all headers into the C:\Projekte\marble-release\include\marble|astro dir flat. Also from build-marble-release\src\lib\astro|marble.
-
-mkdir %DEPLOYDIR%\include\astro
-xcopy /I /F /Y %%APROJECTS%\marble\src\lib\astro\*.h %DEPLOYDIR%\include\astro
 
 mkdir %DEPLOYDIR%\include\marble
+
 xcopy /I /F /Y %BUILDDIR%\src\lib\marble\marble_export.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\*\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\blendings\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\cloudsync\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\declarative\*.h %DEPLOYDIR%\include\marble
-
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\data\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\graphicsitem\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\handlers\kml\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\handlers\dgml\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\parser\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\scene\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\writer\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\writers\kml\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\geodata\writers\dgml\*.h %DEPLOYDIR%\include\marble
-
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\graphicsview\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\htmlfeatures\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\layers\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\osm\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\projections\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\routing\*.h %DEPLOYDIR%\include\marble
-xcopy /I /F /Y %APROJECTS%\marble\src\lib\marble\webpopup\*.h %DEPLOYDIR%\include\marble
+IF ERRORLEVEL 1 goto :err
 
 popd
 
